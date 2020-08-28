f49293138f54801668e7aee832822806c8923d4f
salvio-esposito <salvatore.esposito78@gmail.com>
2020-08-28 20:12:59 +0000

    fixed assets_path function helper

--- code/config/bootstrap.php
+++ code/config/bootstrap.php
@@ -6,7 +6,7 @@ use Slim\Factory\AppFactory;
 
 /* Dot-env */
 $dotenv = new Dotenv();
-$dotenv->load(CURRENT_PATH . '.env');
+$dotenv->load( CURRENT_PATH . '.env' );
 
 /* Slim and Container*/
 $container = new DI\Container();
@@ -16,16 +16,16 @@ $app = AppFactory::create();
 /* Twig */
 $cache = strtolower($_ENV['TWIG_CACHE'])!=='false' ? $_ENV['TWIG_CACHE'] : FALSE;
 
-$loader = new \Twig\Loader\FilesystemLoader(CURRENT_PATH .  'src/templates');
+$loader = new \Twig\Loader\FilesystemLoader( CURRENT_PATH .  'src/templates' );
 $twig = new \Twig\Environment($loader, [
     'cache' => $cache,
     'debug' => $_ENV['TWIG_DEBUG']
 ]);
-$function = new \Twig\TwigFunction('assets_path', function () {
-    return $_ENV['ASSETS_PATH'];
+
+$function = new \Twig\TwigFunction('assets_path', function ($file) {
+    return sprintf("./%s/%s", $_ENV['ASSETS_PATH'], $file);
 });
 $twig->addFunction($function);
 
 /* set container objects */
-$container->set('view' , $twig);
-$container->set('assets_path', $_ENV['ASSETS_PATH']);
+$container->set( 'view' , $twig );
