dc16de715de0e91982fef2a8743b558a91715248
salvio-esposito <salvatore.esposito78@gmail.com>
2020-08-28 10:45:05 +0000

    first commit

---
+++ code/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="/dist/app.css">
+  </head>
+  <body>
+    test
+  </body>
+  <script src="/dist/app.js"></script>
+</html>
