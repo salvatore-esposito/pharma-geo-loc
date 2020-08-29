df2a4e7bbef72475a0fc904d8917c5e71e82fe95
salvio-esposito <salvatore.esposito78@gmail.com>
2020-08-29 22:36:33 +0000

    Refactored PharmacyFactory class

--- code/app/PharmacyFactory.php
+++ code/app/PharmacyFactory.php
@@ -3,27 +3,36 @@ declare(strict_types=1);
 
 namespace GeoPharmsLoc;
 
+// note that this class has two responsibilities: make Pharmacies objects and put them
+// in an array repository
 class PharmacyFactory
 {
-    public static function createPharmsArrayFromJSON() : array
+    public static function createPharmacyObject($name, $coords) : array
     {
-      $pharmacies = [];
+      return new Pharmacy( $name, $coords );
+    }
 
-      //Put this as arguments
-      $jsonPayload = CampaniaPharmacies::getPayload();
 
-      $coords = new GeoCoordinate([ 'lon' => 0, 'lat' => 0]);
+    public static function getPharmsArray() : array
+    {
+      {
+        $pharmacies = [];
 
-      foreach ($jsonPayload as $pharmacy) {
+        //Put this as an arguments
+        $jsonPayload = CampaniaPharmacies::getPayload();
 
-        $coords->setLon($pharmacy['geometry']['GeoCoordinate'][0]);
-        $coords->setLat($pharmacy['geometry']['GeoCoordinate'][1]);
+        $coords = new GeoCoordinate([ 'lon' => 0, 'lat' => 0]);
 
-        $pharmacies[] = new Pharmacy(
-                        $pharmacy['properties']['Descrizione'],
-                        $coords
-                      );
+        foreach ($jsonPayload as $pharmacy) {
+
+          $coords->setLon($pharmacy['geometry']['GeoCoordinate'][0]);
+          $coords->setLat($pharmacy['geometry']['GeoCoordinate'][1]);
+
+          $pharmacies[] = self::createPharmacyObject(
+                                               $pharmacy['properties']['Descrizione'],
+                                               $coords);
+        }
+        return $pharmacies;
       }
-      return $pharmacies;
     }
 }
