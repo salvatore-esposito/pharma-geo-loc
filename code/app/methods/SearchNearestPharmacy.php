fcb82188ef7893abaf387b65797767f70dabc964
salvio-esposito <salvatore.esposito78@gmail.com>
2020-08-30 10:22:08 +0000

    added InvalidException to SearchNearestPharmacy

--- code/app/methods/SearchNearestPharmacy.php
+++ code/app/methods/SearchNearestPharmacy.php
@@ -7,12 +7,26 @@ use GeoPharmsLoc\MethodInterface as MethodInterface;
 use GeoPharmsLoc\Geolocalizator as Geolocalizator;
 use GeoPharmsLoc\PharmacyFactory as PharmacyFactory;
 use GeoPharmsLoc\EarthDistanceCalculator as EarthDistanceCalculator;
+use GeoPharmsLoc\GeoCoordinate as GeoCoordinate;
 
 final class SearchNearestPharmacy implements MethodInterface
 {
   public function operation( array $params = [] ) : array
   {
+    if(!(array_key_exists('range', $params) &&
+         array_key_exists('currentLocation', $params) &&
+         array_key_exists('limit', $params))
+      )
+    {
+      throw new \InvalidArgumentException('$params must have range,currentLocation and limit keys!');
+    }
+
     extract($params);
+
+    if(!($currentLocation instanceof GeoCoordinate ))
+    {
+      throw new \InvalidArgumentException('currentLocation must be of GeoCoordinate type!');
+    }
 
     $pharmacies = PharmacyFactory::getPharmsArray();
 
